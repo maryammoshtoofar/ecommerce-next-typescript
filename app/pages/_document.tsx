4134422661fe134f040379aab86e64c8e3730c3c
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2023-11-18 16:25:42 +0330

    build:update node and npm versions

---
+++ app/pages/_document.tsx
@@ -0,0 +1,25 @@
+// This custom document file is a good place to include links to font files,
+//  analytics SDK links, or any script that you think should be available globally
+//  such as <Main /> and <NextScript />
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default MyDocument;
