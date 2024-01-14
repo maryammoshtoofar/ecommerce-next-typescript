1ba34b39bb369c794071b0964bd884438e406548
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2024-01-14 12:38:12 +0330

    chore:clean up tw componenets

--- app/components/widgets/reserve-table/ReserveTable.tsx
+++ app/components/widgets/reserve-table/ReserveTable.tsx
@@ -14,22 +14,18 @@ import { TbReservedLine } from 'react-icons/tb';
 import Button from '@/app/components/base/button/Button';
 const ReserveTable = (props: UIComponent) => {
   return (
-    <div className="form-glow relative flex w-full flex-col gap-20 bg-coffee-640 px-5 py-10 sm:flex-row sm:items-center sm:justify-between lg:gap-40 xl:gap-80 xl:px-32">
-      <h1 className="gradient-text absolute -top-10 left-5 whitespace-nowrap font-dancing text-5xl capitalize">
+    <section className="reserve-table-section">
+      <h1 className="reserve-table-title">
         reserve <span className="hidden sm:inline">a table</span>
       </h1>
-      <Image
-        src={cupAndPeople}
-        alt="reserve"
-        className="max-sm:w-full sm:max-md:w-1/2"
-      />
-      <form className="flex grow flex-col gap-6 ">
+      <Image src={cupAndPeople} alt="reserve" className="reserve-table-img" />
+      <form className="reserve-table-form ">
         <Input
           type="text"
           placeholder="Date"
           component={<MdOutlineCalendarToday />}
         />
-        <div className="flex flex-col items-center gap-6 md:flex-row md:gap-3">
+        <div className="reserve-table-inputs-flexed">
           <Input
             type="text"
             placeholder="Time"
@@ -47,7 +43,7 @@ const ReserveTable = (props: UIComponent) => {
           placeholder="Your Phone"
           component={<MdOutlinePhoneIphone />}
         />
-        <div className="md: flex w-full flex-col items-center gap-6 md:flex-row">
+        <div className="reserve-table-inputs-flexed">
           <Input
             type="text"
             placeholder="Your Email"
@@ -55,13 +51,13 @@ const ReserveTable = (props: UIComponent) => {
           />
           <Button
             label="Reserve"
-            tailwind="py-2 text-lg w-1/2"
+            tailwind="reserve-table-btn"
             mode="secondary"
             component={<TbReservedLine />}
           />
         </div>
       </form>
-    </div>
+    </section>
   );
 };
 
