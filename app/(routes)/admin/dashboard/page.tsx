5f49bca5c1b89d9550122efbc7be50c488f463c8
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2024-02-07 15:03:59 +0330

    UI:add admin dashboard card widget

--- app/(routes)/admin/dashboard/page.tsx
+++ app/(routes)/admin/dashboard/page.tsx
@@ -1,11 +1,36 @@
+import Card from '@/app/_components/widgets/admin-dashboard-card/admin-dashboard-card';
 import React from 'react';
-
+import { FiShoppingBag } from 'react-icons/fi';
+import { FiUser } from 'react-icons/fi';
+import { FiShoppingCart } from 'react-icons/fi';
 type Props = {};
 
 const Dashboard = (props: Props) => {
   return (
-    <section className="col-span-12 sm:col-span-9 row-start-2 w-full ">
-      ADMIN DASHBOARD
+    <section className="col-span-12 row-start-2 w-full sm:col-span-9 ">
+      <div className="flex w-full justify-between gap-4">
+        <Card
+          title="total orders"
+          percentage={-2.33}
+          amount={21375}
+          component={<FiShoppingBag />}
+          progress={70}
+        />
+        <Card
+          title="new customers"
+          percentage={+32.4}
+          amount={1012}
+          component={<FiUser />}
+          progress={65}
+        />
+        <Card
+          title="total sales"
+          percentage={+25}
+          amount={24254}
+          component={<FiShoppingCart />}
+          progress={55}
+        />
+      </div>
     </section>
   );
 };
--- app/_components/widgets/admin-profile-section/admin-profile-section.tsx
+++ app/_components/widgets/admin-profile-section/admin-profile-section.tsx
@@ -5,14 +5,18 @@ import { FaRegBell } from 'react-icons/fa6';
 const ProfileSection = () => {
   return (
     <article className="col-span-3 flex items-center justify-evenly text-coffee-360 sm:col-span-6 xl:col-span-4">
-      <Image
-        src={profilePhoto}
-        alt="Profile"
-        className="w-1/2 rounded-lg sm:w-auto"
-      />
-      <div>
-        <h3 className="hidden  text-coffee-460 sm:block">Admin</h3>
-        <span className="hidden text-sm font-bold sm:inline">Lee Robinson</span>
+      <div className="flex gap-2">
+        <Image
+          src={profilePhoto}
+          alt="Profile"
+          className="w-1/2 rounded-lg sm:w-auto"
+        />
+        <div>
+          <h3 className="hidden  text-coffee-460 sm:block">Admin</h3>
+          <span className="hidden text-sm font-bold sm:inline">
+            Lee Robinson
+          </span>
+        </div>
       </div>
       <FaRegBell className="cursor-pointer text-xl hover:text-coffee-70 sm:text-3xl" />
     </article>
