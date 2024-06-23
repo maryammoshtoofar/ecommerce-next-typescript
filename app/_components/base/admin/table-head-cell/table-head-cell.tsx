856ae5308edae14be6d1fa912dfbfb3348b8d742
maryam moshtoofar <maryammoshtoofar@gmail.com>
2024-06-23 21:18:35 +0330

    UI:fix admin panel products table thead cells

--- app/_components/base/admin/table-head-cell/table-head-cell.tsx
+++ app/_components/base/admin/table-head-cell/table-head-cell.tsx
@@ -1,14 +1,12 @@
 import { IconType } from 'react-icons';
 type Props = {
-  component?: IconType;
   children: React.ReactNode;
 };
 
-export const TableHeadCell = ({ children, component: Icon }: Props) => {
+export const TableHeadCell = ({ children }: Props) => {
   return (
     <th className="whitespace-nowrap border border-coffee-640 p-1">
-      {Icon && <Icon className="inline md:hidden" />}
-      <span className="hidden md:inline">{children}</span>
+      {children}
     </th>
   );
 };
--- app/_components/widgets/admin/tables/products-table/table.tsx
+++ app/_components/widgets/admin/tables/products-table/table.tsx
@@ -9,32 +9,26 @@ import {
 } from 'react-icons/md';
 import { GoNumber } from 'react-icons/go';
 import { FaRegStar } from 'react-icons/fa';
-import { TableHeadCell, Checkbox } from '@/app/_components/base';
+import { TableHeadCell as Th, Checkbox } from '@/app/_components/base';
 
 export const ProductsTable = (props: UIComponent) => {
   return (
-    <div className='w-full overflow-scroll'>
+    <div className="w-full overflow-scroll">
       <table className="table-auto bg-coffee-10 font-semibold text-coffee-240">
         <thead className=" text-2xl text-coffee-320 md:text-xl">
           <tr className="bg-coffee-30 p-4 py-12 font-dancing capitalize">
-            <TableHeadCell>
+            <Th>
               <Checkbox />
-            </TableHeadCell>
-            <TableHeadCell component={MdOutlineImage}>image</TableHeadCell>
-            <TableHeadCell component={MdOutlineSubtitles}>name</TableHeadCell>
-            <TableHeadCell component={MdOutlineCategory}>
-              category
-            </TableHeadCell>
-            <TableHeadCell component={MdOutlineCategory}>
-              subcategory
-            </TableHeadCell>
-            <TableHeadCell component={MdOutlineAttachMoney}>
-              price
-            </TableHeadCell>
-            <TableHeadCell component={MdOutlineInventory}>stock</TableHeadCell>
-            <TableHeadCell component={GoNumber}>quantity</TableHeadCell>
-            <TableHeadCell component={FaRegStar}>rating</TableHeadCell>
-            <TableHeadCell component={MdGridOn}>actions</TableHeadCell>
+            </Th>
+            <Th>image</Th>
+            <Th>name</Th>
+            <Th>category</Th>
+            <Th>subcategory</Th>
+            <Th>price</Th>
+            <Th>stock</Th>
+            <Th>quantity</Th>
+            <Th>rating</Th>
+            <Th>actions</Th>
           </tr>
         </thead>
         <tbody className="overflow-x-scroll">{props.children}</tbody>
