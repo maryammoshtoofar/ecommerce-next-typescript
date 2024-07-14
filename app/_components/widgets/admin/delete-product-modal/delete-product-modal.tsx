845e387bc48d3ce863128e3357ec22a313b1e7c3
maryam moshtoofar <maryammoshtoofar@gmail.com>
2024-07-14 09:27:38 +0330

    feat:delete product close modal after deletion

--- app/_components/widgets/admin/delete-product-modal/delete-product-modal.tsx
+++ app/_components/widgets/admin/delete-product-modal/delete-product-modal.tsx
@@ -23,22 +23,24 @@ export const DeleteProductModal = (props: Props) => {
     <Overlay tailwind="items-center">
       <div className="flex w-full flex-col rounded-lg  bg-white p-4 opacity-100 smmobile:w-3/4 sm:w-1/2 md:w-1/3 lg:w-1/4">
         <Link href={INVENTORY} className="self-end">
-          <MdClose />
+          <MdClose className="text-coffee-320" />
         </Link>
         <div className="flex flex-col items-center justify-center gap-4 capitalize">
-          <p className="font-bold text-yellow-600">
+          <p className="font-bold text-coffee-320">
             are you sure you want to delete this product?
           </p>
           <div className="flex gap-4 px-6">
-            <Button
-              onClick={() => handleDeleteProduct(props.id)}
-              label="delete"
-              tailwind="font-bold bg-red-600"
-            />
+            <Link href={INVENTORY}>
+              <Button
+                onClick={() => handleDeleteProduct(props.id)}
+                label="delete"
+                tailwind="font-bold bg-red-600"
+              />
+            </Link>
             <Link href={INVENTORY}>
               <Button
                 label="cancel"
-                tailwind="font-bold bg-coffee-200 text-coffee-310"
+                tailwind="font-bold bg-coffee-160 text-coffee-310"
               />
             </Link>
           </div>
